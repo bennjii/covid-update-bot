2b39c3d369099f7e82957ee96421fa4e70d651bb
UnRealG <benwhite2413@gmail.com>
2020-05-01 15:29:59 +1200

    CHANGE • Privatized Variables

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const botSettings = require("./BotSettings.json");
 const Discord = require("discord.js");
 const prefix = botSettings.prefix;
 const hook = new Discord.WebhookClient('webhook id', 'webhook token');
 const bot = new Discord.Client({disableEveryone: true});
+const token = process.env.token;
 
 bot.on("ready", async () => {
     console.log('COVID-UPDATE is ready!');
@@ -20,5 +20,5 @@ bot.on('ready', () => {
     bot.user.setActivity('The World', { type: 'WATCHING' });
 });
 
-bot.login(botSettings.token);
+bot.login(token);
 // https://discordapp.com/oauth2/authorize?client_id=705614558261280778&scope=bot
